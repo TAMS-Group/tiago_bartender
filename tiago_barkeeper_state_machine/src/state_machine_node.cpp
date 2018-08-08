a6172fd0460b92e76bc01baf4cf0bb8647d1d706
Benjamin Scholz <12scholz@cloak.mafiasi.de>
2018-08-08 14:41:53 +0200

    state machine integration
    tiago can now finish a very simple round where it drives from
    one place to the other, but does not grasp anything yet and also
    is not able to recognize persons.

--- tiago_barkeeper_state_machine/src/state_machine_node.cpp
+++ tiago_barkeeper_state_machine/src/state_machine_node.cpp
@@ -2,8 +2,12 @@
 #include <functional>
 #include <std_msgs/String.h>
 #include <tiago_barkeeper_navigation/MoveToTargetAction.h>
+#include <tiago_barkeeper_navigation/FindClosestTargetAction.h>
 #include <tiago_bartender_mtc/PickBottleAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <std_srvs/Empty.h>
+#include <geometry_msgs/PointStamped.h>
+#include <tiago_barkeeper_behavior/MenuOrderAction.h>
 
 struct StateMachine
 {
@@ -14,20 +18,27 @@ struct StateMachine
 
   void state_init()
   {
-    state = &StateMachine::state_watch;
-  }
-  
-  void state_watch()
-  {
-    ROS_INFO("watching");
+    ROS_INFO("idling");
     ros::NodeHandle node;
+
+    // start idle behavior
+    ros::ServiceClient client = node.serviceClient<std_srvs::Empty>("switch_simple_idle");
+    std_srvs::Empty srv;
+    if (!client.call(srv))
+    {
+      ROS_ERROR("Failed to call service switch_simple_idle");
+      state = &StateMachine::state_init;
+      return;
+    }
+
+    // Wait for a person to be detected
     bool received = false;
-    std::string name;
-    auto selection = node.subscribe<std_msgs::String>("/selection", 1, boost::function<void(std_msgs::String)>(
-      [&](std_msgs::String s) {
-        ROS_INFO("selection");
+    geometry_msgs::PointStamped person_position;
+    auto selection = node.subscribe<geometry_msgs::PointStamped>("/person_detection", 1, boost::function<void(geometry_msgs::PointStamped)>(
+      [&](geometry_msgs::PointStamped p) {
+        ROS_INFO("person pose");
         received = true;
-        name = s.data;
+        person_position = p;
       }
     ));
     while(true)
@@ -35,22 +46,60 @@ struct StateMachine
       ros::Duration(0.1).sleep();
       if(received) break;
     }
-    bottle_name = name;
-    //state = &StateMachine::state_move_to_bottle;
-    state = [name](StateMachine* m) { m->state_move_to_bottle(name); };
+
+    // stop idle behavior
+    if (!client.call(srv))
+    {
+      ROS_ERROR("Failed to call service switch_simple_idle");
+      state = &StateMachine::state_init;
+      return;
+    }
+    sleep(0.1);
+
+    state = [person_position](StateMachine* m) { m->state_take_order(person_position); };
+  }
+
+  void state_take_order(const geometry_msgs::PointStamped person_position)
+  {
+    ROS_INFO("Taking order");
+    actionlib::SimpleActionClient<tiago_barkeeper_behavior::MenuOrderAction> client("take_order_action", true);
+    client.waitForServer();
+    tiago_barkeeper_behavior::MenuOrderGoal goal;
+    goal.person_position = person_position;
+    client.sendGoal(goal);
+    while(!client.waitForResult(ros::Duration(5.0)))
+      ROS_INFO("Waiting for menu order result.");
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO("Successfully took order.");
+    else
+    {
+      ROS_ERROR("Taking order aborted.");
+      state = [person_position](StateMachine* m) { m->state_take_order(person_position); };
+      return;
+    }
+
+    auto result = client.getResult();
+    std::string bottle_name = result->selection;
+
+    last_person_position_ = person_position;
+
+    state = [bottle_name](StateMachine* m) { m->state_move_to_bottle(bottle_name); };
   }
 
   void state_move_to_bottle(const std::string& bottle_name)
   {
-    move_to_target(bottle_name);
-    state = [bottle_name](StateMachine* m) { m->state_grasp_bottle(bottle_name); };
+    bool success = move_to_target(bottle_name);
+    if(success)
+      state = [bottle_name](StateMachine* m) { m->state_grasp_bottle(bottle_name); };
+    else
+      state = [bottle_name](StateMachine* m) { m->state_move_to_bottle(bottle_name); };
   }
  
   void state_grasp_bottle(const std::string& bottle_name)
   {
     ROS_INFO("grasping");
 
-    actionlib::SimpleActionClient<tiago_bartender_mtc::PickBottleAction> client("tiago_bottle_pick", true);
+    /*actionlib::SimpleActionClient<tiago_bartender_mtc::PickBottleAction> client("tiago_bottle_pick", true);
     client.waitForServer();
     tiago_bartender_mtc::PickBottleGoal goal;
     goal.bottle_id = bottle_name;
@@ -61,6 +110,7 @@ struct StateMachine
       ROS_INFO("Successfully grasped to bottle.");
     else
       ROS_ERROR("Grasping bottle aborted.");
+    */
 
     state = &StateMachine::state_move_to_glass;
   }
@@ -68,8 +118,14 @@ struct StateMachine
   void state_move_to_glass()
   {
     ROS_INFO("moving to glass");
-    ros::Duration(1).sleep();
-    state = &StateMachine::state_pour;
+    geometry_msgs::PoseStamped person_pose;
+    person_pose.header = last_person_position_.header;
+    person_pose.pose.position = last_person_position_.point;
+    bool success = find_closest_target("glass", person_pose);
+    if(success)
+      state = &StateMachine::state_pour;
+    else
+      state = &StateMachine::state_move_to_glass;
   }
 
   void state_pour()
@@ -82,20 +138,23 @@ struct StateMachine
   void state_move_back_to_shelf()
   {
     ROS_INFO("moving back to shelf");
-    ros::Duration(1).sleep();
-    state = &StateMachine::state_put_back_bottle;
+    bool success = move_to_pose(last_robot_target_pose_);
+    if(success)
+      state = &StateMachine::state_put_back_bottle;
+    else
+      state = &StateMachine::state_move_back_to_shelf;
   }
 
   void state_put_back_bottle()
   {
     ROS_INFO("putting bottle back");
     ros::Duration(1).sleep();
-    state = &StateMachine::state_watch;
+    state = &StateMachine::state_init;
   }
  
-  void move_to_target(const std::string& target_name)
+  bool move_to_target(const std::string& target_name)
   {
-    ROS_INFO("moving to bottle %s", bottle_name.c_str());
+    ROS_INFO("moving to target %s", target_name.c_str());
     actionlib::SimpleActionClient<tiago_barkeeper_navigation::MoveToTargetAction> client("move_to_target", true);
     client.waitForServer();
     tiago_barkeeper_navigation::MoveToTargetGoal goal;
@@ -104,9 +163,61 @@ struct StateMachine
     while(!client.waitForResult(ros::Duration(5.0)))
       ROS_INFO("Waiting for move_to_target result.");
     if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_INFO("Successfully moved to bottle.");
+    {
+      ROS_INFO("Successfully moved to target.");
+      last_robot_target_pose_ = client.getResult()->pose_result;
+      return true;
+    }
     else
-      ROS_ERROR("Moving to bottle aborted.");
+    {
+      ROS_ERROR("Moving to target aborted.");
+      return false;
+    }
+  }
+
+  bool find_closest_target(const std::string& type_name, const geometry_msgs::PoseStamped& pose)
+  {
+    ROS_INFO("Finding closest target of type %s", type_name.c_str());
+    actionlib::SimpleActionClient<tiago_barkeeper_navigation::FindClosestTargetAction> client("find_closest_target", true);
+    client.waitForServer();
+    tiago_barkeeper_navigation::FindClosestTargetGoal goal;
+    goal.target_type = type_name;
+    goal.target_pose = pose;
+    client.sendGoal(goal);
+    while(!client.waitForResult(ros::Duration(5.0)))
+      ROS_INFO("Waiting for find_closest_target result.");
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Successfully found and moved to target.");
+      return true;
+    }
+    else
+    {
+      ROS_ERROR("Find closest target aborted.");
+      return false;
+    }
+  }
+
+  bool move_to_pose(const geometry_msgs::PoseStamped pose)
+  {
+    ROS_INFO("moving to pose.");
+    actionlib::SimpleActionClient<tiago_barkeeper_navigation::MoveToTargetAction> client("move_to_target", true);
+    client.waitForServer();
+    tiago_barkeeper_navigation::MoveToTargetGoal goal;
+    goal.target_pose = pose;
+    client.sendGoal(goal);
+    while(!client.waitForResult(ros::Duration(5.0)))
+      ROS_INFO("Waiting for move_to_target result.");
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Successfully moved to target.");
+      return true;
+    }
+    else
+    {
+      ROS_ERROR("Moving to target aborted.");
+      return false;
+    }
   }
   
   void run()
@@ -119,6 +230,10 @@ struct StateMachine
       //state = s;
     }
   }
+
+  geometry_msgs::PointStamped last_person_position_;
+  // pose the robot was in when it last grasped the bottle
+  geometry_msgs::PoseStamped last_robot_target_pose_;
 
 };
 
