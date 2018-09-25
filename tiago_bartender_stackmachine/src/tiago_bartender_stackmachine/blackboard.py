edd15c851618f124ecd1731640fb2f40965860b4
v4hn <me@v4hn.de>
2018-09-25 18:54:31 +0200

    fix simple errors in blackboard

--- tiago_bartender_stackmachine/src/tiago_bartender_stackmachine/blackboard.py
+++ tiago_bartender_stackmachine/src/tiago_bartender_stackmachine/blackboard.py
@@ -104,10 +104,9 @@ class Blackboard:
 
     def person_detections_cb(self, detections):
         #TODO: Check if customer in front of bar
-        if detections:
+        if len(detections.detections) > 0:
             self.person_detected = True
-        for p in detections:
-            self.person_position = p.position
+            self.person_position = detections.detections[0].position
 
     def reset_for_next_bottle(self):
         self.arrived_at_pouring_position = False
@@ -186,13 +185,13 @@ class Blackboard:
             print("Service call failed: %s"%e)
 
     def rotate_point(origin, point, angle):
-    """
-    Rotate a point counterclockwise by a given angle around a given origin.
-    The angle should be given in radians.
-    """
-    ox, oy = origin
-    px, py = point
+        """
+        Rotate a point counterclockwise by a given angle around a given origin.
+        The angle should be given in radians.
+        """
+        ox, oy = origin
+        px, py = point
 
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-    return qx, qy
+        qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+        qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+        return qx, qy
