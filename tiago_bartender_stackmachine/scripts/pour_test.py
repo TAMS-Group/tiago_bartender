c251d1d907e288e94ef293b2062d4eb39500f857
Benjamin Scholz <12scholz@cloak.mafiasi.de>
2018-09-26 16:41:17 +0200

    add test stackmachine for pouring

---
+++ tiago_bartender_stackmachine/scripts/pour_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+
+from bitbots_stackmachine.stack_machine import StackMachine
+from tiago_bartender_stackmachine.pour_decisions import Init
+from tiago_bartender_stackmachine.blackboard import Blackboard
+
+class TiagoBartender:
+    def __init__(self):
+        rospy.init_node('tiago_bartender_stackmachine')
+
+        self.blackboard = Blackboard()
+        self.stackmachine = StackMachine(self.blackboard, "/debug_stackmachine")
+        rospy.sleep(1) # needed to prevent race condition with initilizing the publisher of the stack machine. without the vizualization will not have data of the init
+    	self.stackmachine.set_start_element(Init)
+        #TODO self.pause_card_subscriber(..., self.pause_card_cb)
+        #TODO self.redo_card_subscriber(..., self.redo_card_cb)
+
+        self.run()        
+
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.stackmachine.update()
+        print("exit stackmachine")
+
+    def pause_card_cb(self, msg):
+        self.blackboard.was_pause_card_shown = True
+
+    def redo_card_cb(self, msg):
+        self.blackboard.redo_requested = True
+
+
+def main():
+    bartender = TiagoBartender()
+    bartender.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tiago_bartender_stackmachine/src/tiago_bartender_stackmachine/pour_decisions.py
@@ -0,0 +1,70 @@
+import rospy
+import actionlib
+from bitbots_stackmachine.abstract_decision_element import AbstractDecisionElement
+from bitbots_stackmachine.sequence_element import SequenceElement
+from .actions import IdleMoveAround, WaitingToResume, MoveToCustomer, SayRepeatOrder, SayNoMenuFoundRepeat, SayOrderConfirmed, ObserveOrder, LookAtCustomer, SayPleaseOrder, LookAtMenu, MoveToBottle, LookAtBottle, MoveToPouringPosition, PourLiquid, Wait, PickUpBottle, SayDrinkFinished, LookForward, LookForCustomer, UpdateBottlePose, GetNextBottle, PlaceBottle, MoveToBottlePose, SayBottleNotFound, ExtendTorso
+from tiago_bartender_msgs.msg import PourAction, PickAction, MoveToTargetAction, TakeOrderAction
+from control_msgs.msg import FollowJointTrajectoryAction
+from pal_interaction_msgs.msg import TtsAction
+from move_base_msgs.msg import MoveBaseAction
+from tiago_bartender_msgs.srv import LookAt
+
+
+# @BitBots: see IMPROVE tags
+
+
+class Init(AbstractDecisionElement):
+    """
+    Initializes the tiago bartender demo.
+    Verifies ros connectivity
+    """
+    def __init__(self, blackboard, _):
+        super(AbstractDecisionElement, self).__init__(blackboard)
+        # set variables needed for the test
+        blackboard.current_bottle = 'coke'
+        blackboard.get_next_bottle = False
+
+        self.initilized = False
+
+    def perform(self, blackboard, reevaluate=False):
+        #if not blackboard.move_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'move_to_target')
+        #if not blackboard.torso_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'torso_controller/follow_joint_trajectory')
+        #if not blackboard.tts_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'tts')
+        #if not blackboard.take_order_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'menu/take_order')
+        #if not blackboard.move_base_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'move_base')
+        #if not blackboard.pick_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'pick')
+        #if not blackboard.pour_action_client.wait_for_server(0.01):
+        #    return self.push(WaitForRos, 'pour')
+        #if not rospy.wait_for_service('head_controller/look_at_service', 0.01):
+        #    return self.push(WaitForRos, 'head_controller/look_at_service')
+        return self.push(InPouringPosition)
+
+class InPouringPosition(AbstractDecisionElement):
+    """
+    Goes to pouring position
+    """
+    def __init__(self, blackboard, _):
+        super(AbstractDecisionElement, self).__init__(blackboard)
+        blackboard.last_redoable = blackboard.POUR
+
+    def perform(self, blackboard, reevaluate=False):
+        if blackboard.redo_requested and blackboard.last_redoable == blackboard.POUR:
+            return self.push(MoveToPouringPosition)
+
+        if blackboard.get_next_bottle:
+            print('pouring succeeded')
+            rospy.signal_shutdown('done')
+
+        if blackboard.arrived_at_pouring_position:
+            return self.push_action_sequence(SequenceElement, [ExtendTorso, PourLiquid, Wait], [None, None, 5])
+        else:
+            return self.push(MoveToPouringPosition)
+
+        def get_reevaluate(self):
+            return True
