3b3982773b17402e5e0ae1707148ba760a70e9b4
Philipp Ruppel <ruppel@informatik.uni-hamburg.de>
2018-10-04 14:34:37 +0200

    larger target marker

--- tiago_bartender_navigation/src/move_to_target_node.cpp
+++ tiago_bartender_navigation/src/move_to_target_node.cpp
@@ -284,13 +284,13 @@ public:
       marker.type = visualization_msgs::Marker::SPHERE;
       marker.action = visualization_msgs::Marker::ADD;
       marker.pose = target_pose.pose;
-      marker.scale.x = 0.1;
-      marker.scale.y = 0.1;
-      marker.scale.z = 0.1;
+      marker.scale.x = 0.5;
+      marker.scale.y = 0.5;
+      marker.scale.z = 0.5;
       marker.color.a = 1.0;
-      marker.color.r = 0.0;
+      marker.color.r = 1.0;
       marker.color.g = 0.0;
-      marker.color.b = 1.0;
+      marker.color.b = 0.0;
       vis_pub_.publish(marker);
     }
 
